6bb5bc0b9c6a3bd2ac0f92bb0275b6ee8b1a83ba
cleanDev-win <beautifulsky1991@hotmail.com>
2020-12-11 18:19:11 +0300

    add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,13 @@ import 'firebase/storage';
 import 'firebase/functions';
 
 const config = {
-  apiKey: process.env.REACT_APP_FIRE_BASE_KEY,
-  authDomain: process.env.REACT_APP_FIRE_BASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIRE_BASE_DB_URL,
-  projectId: process.env.REACT_APP_FIRE_BASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIRE_BASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIRE_BASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIRE_BASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIRE_BASE_MEASURMENT_ID,
+  apiKey: 'AIzaSyANkU9YwZHBQHo919vII2DkO9Y83nIbuXQ',
+  authDomain: 'carpeprod-b158f.firebaseapp.com',
+  databaseURL: 'https://carpeprod-b158f.firebaseio.com',
+  projectId: 'carpeprod-b158f',
+  storageBucket: 'carpeprod-b158f.appspot.com',
+  messagingSenderId: '361305835202',
+  appId: '1:361305835202:web:68f6837ccfd6137a',
 };
 
 firebase.initializeApp(config);
